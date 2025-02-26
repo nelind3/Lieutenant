f8435ecfca8a4eb7c386b6f7a664e6262fd66f76
nelind <nel.n.lindberg@gmail.com>
2025-02-27 00:09:00 +0100

    Add maven publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     id("java-library")
+    id("maven-publish")
 }
 
 allprojects {
     plugins.apply("java-library")
+    plugins.apply("maven-publish")
 
     group = "dk.nelind"
     version = "${rootProject.properties["version"]}"
@@ -22,5 +24,27 @@ allprojects {
     repositories {
         mavenCentral()
         maven("https://libraries.minecraft.net/")
+    }
+
+    publishing {
+        repositories {
+            val username = "NELIND_MAVEN_USERNAME".let { System.getenv(it) ?: findProperty(it) }?.toString()
+            val password = "NELIND_MAVEN_PASSWORD".let { System.getenv(it) ?: findProperty(it) }?.toString()
+            if (username != null && password != null) {
+                maven("https://maven.nelind.dk/releases") {
+                    name = "NelindReleases"
+                    credentials {
+                        this.username = username
+                        this.password = password
+                    }
+                }
+            } else {
+                println("Nelind Maven credentials not present. Repository not added as publication destination")
+            }
+        }
+
+        publications.create<MavenPublication>("main") {
+            from(components["java"])
+        }
     }
 }
